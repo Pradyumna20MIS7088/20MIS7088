63047b9fb1458bef51ba36098f754cf5eaec2047
GUNDA VENKATA NAGA CHAITHANYA PRADYUMNA <79201653+Pradyumna20MIS7088@users.noreply.github.com>
2021-02-27 15:21:41 +0530

    Create  Sum.java

---
+++ Sum.java
@@ -0,0 +1,11 @@
+# 20MIS7088
+import java.lang.*; 
+public class Main { 
+    public static void main(String[] args) 
+    { 
+        int a = 62; 
+        int b = 18; 
+        // It will return the sum of two arguments. 
+        System.out.println("The sum is =" + Integer.sum(a, b)); 
+    } 
+} 
